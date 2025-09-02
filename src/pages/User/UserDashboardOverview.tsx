aea7d994ab61acca66a89fa686fe2c0e555a9aaa
imran-khan-dev <emrankhanniloy@gmail.com>
2025-09-02 21:51:27 +0600

    Add: dashboard overoview quick actions

--- src/pages/User/UserDashboardOverview.tsx
+++ src/pages/User/UserDashboardOverview.tsx
@@ -1,9 +1,25 @@
 import { DashboardOverviewData } from "@/components/DashboardOverviewData";
+import { CashOutForm } from "@/components/modules/user/Cashout";
+import { SendMoneyForm } from "@/components/modules/user/SendMoney";
 
 export default function UserOverview() {
   return (
-    <div>
+    <div className="space-y-6">
+      {/* Overview Section */}
       <DashboardOverviewData />
+
+      {/* Actions Section */}
+      <div className="p-6 grid grid-cols-1 lg:grid-cols-2 gap-6">
+        {/* Cash Out */}
+        <div className="bg-white shadow rounded-2xl p-6">
+          <CashOutForm />
+        </div>
+
+        {/* Send Money */}
+        <div className="bg-white shadow rounded-2xl p-6">
+          <SendMoneyForm />
+        </div>
+      </div>
     </div>
   );
 }
